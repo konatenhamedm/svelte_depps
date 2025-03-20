ce0c6d357f3b194bbe5521d25fd32cd691630b48
konate <konatenhamed@gmail.com>
2025-03-20 00:10:48 +0000

    save

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,29 @@
-import adapter from '@sveltejs/adapter-static'; // Utilisez adapter-static
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html', // Fallback pour les routes dynamiques
-      precompress: false,
-    }),
-    alias: {
-      $components: path.resolve('./src/components'),
-      // Ajoutez d'autres alias si nécessaire
-    },
-    prerender: {
-      crawl: true, // Active le crawl des liens internes
-      entries: ['*'], // Prérend toutes les routes
-    },
-  },
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			$components: path.resolve('./src/components'),
+			// Ajoutez d'autres alias si nécessaire
+		  },
+		  prerender: {
+			crawl: true, // Active ou désactive le crawl des liens internes
+			entries: ['*'] // Prérend toutes les routes
+		  }
+	},
+
 };
 
 export default config;
