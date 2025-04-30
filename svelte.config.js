8ef2b17380a5e98483ab4db9ef64366e7e5925c2
konatenhamedm <konatenhamed@gmail.com>
2025-04-30 23:19:43 +0000

    fix: Update Svelte adapter to use adapter-node and streamline configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+import adapter from '@sveltejs/adapter-node';
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		alias: {
-			$components: path.resolve('./src/components'),
-			// Ajoutez d'autres alias si nécessaire
-		  },
-		  prerender: {
-			crawl: true, // Active ou désactive le crawl des liens internes
-			entries: ['*'] // Prérend toutes les routes
-		  }
-	},
-
+export default {
+ kit: {
+  adapter: adapter(
+	{out: 'build' 
+		
+	}
+	
+  ),
+  alias: {
+	$components: path.resolve('./src/components'),
+  }
+ }
 };
-
-export default config;
